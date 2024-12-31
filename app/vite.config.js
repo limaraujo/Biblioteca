a5eb8d04556dfec4ec6784e7c2f61eb419bcc901
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-12-30 22:33:47 -0300

    outras coisas

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Biblioteca/",  // O nome do seu repositório no GitHub
+  base: '/Biblioteca/', // Caminho base do repositório no GitHub Pages
   build: {
-    outDir: 'dist',  // Certifique-se de que os arquivos gerados fiquem na pasta 'dist'
-  }
+    outDir: resolve(__dirname, '../docs'), // Define a pasta `docs` na raiz do projeto
+  },
 });

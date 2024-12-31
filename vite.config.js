7a044cad165bd347bacb88f466df25ce51d20bb2
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-12-30 22:58:30 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Biblioteca/', // Caminho base do repositório no GitHub Pages
-  build: {
-    outDir: resolve(__dirname, '../docs'), // Define a pasta `docs` na raiz do projeto
-  },
+  base: '/Biblioteca/', 
 });
